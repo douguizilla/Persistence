4cec945478104557ac8f6b837369639cb0a1032f
douguizilla <douglasgommez@gmail.com>
2021-08-24 17:14:36 -0300

    set view binding

--- app/src/main/java/com/odougle/persistence/MainActivity.kt
+++ app/src/main/java/com/odougle/persistence/MainActivity.kt
@@ -2,10 +2,15 @@ package com.odougle.persistence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.persistence.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
