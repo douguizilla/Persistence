ede586ab417da7c2b33214ad77d660d79718e921
douguizilla <douglasgommez@gmail.com>
2021-08-24 17:26:07 -0300

    set view binding on MainActivity.kt and start to set listeners

--- app/src/main/java/com/odougle/persistence/MainActivity.kt
+++ app/src/main/java/com/odougle/persistence/MainActivity.kt
@@ -12,5 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        binding.btnRead.setOnClickListener {
+            btnReadClick()
+        }
+        binding.btnSave.setOnClickListener {
+            btnSaveClick()
+        }
+
+    }
+
+    private fun btnReadClick() {
+        TODO("Not yet implemented")
+    }
+
+    private fun btnSaveClick() {
+        TODO("Not yet implemented")
     }
 }
